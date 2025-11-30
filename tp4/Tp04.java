b6f74c6df27aceb7533b0bd6f4c0352b2b4456a0
MKA.Web3 <drobymichel@gmail.com>
2025-11-29 21:44:44 -0500

    update m1

--- tp4/Tp04.java
+++ tp4/Tp04.java
@@ -139,9 +139,6 @@ public class Tp04 {
 		int nombreCases = 9; // Nombre de cases à remplir par partie, maximum 20
 
 		Scanner cl = new Scanner(System.in);
-		System.out.println("Salut");
-		System.out.println("MON MESSAGE");
-		System.out.println("yoooo");
 		// Déclarer un tableau pour les points et l'initialiser avec les points
 		// de base inscrits sur la feuille de pointage disponible dans l'énoncé
 		// Déclarer un tableau de tuiles et l'initialiser en appelant genereTableauTuiles()
